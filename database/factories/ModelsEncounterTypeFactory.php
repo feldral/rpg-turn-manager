547161f66cd67c7f84a5cc07cab52362b157ec45
feldral <george.carleton@gmail.com>
2017-09-18 22:35:02 -0700

    encounter type factory

--- database/factories/ModelsEncounterTypeFactory.php
+++ database/factories/ModelsEncounterTypeFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\EncounterType::class, function (Faker $faker) {
     return [
-        //
+        'name'                  => $faker->words($faker->numberBetween(2, 4)),
+        'has_strict_turn_order' => $faker->boolean(),
+        'slug'                  => str_replace(' ', '-', $faker->words()),
+        'description'           => $faker->sentences(3),
     ];
 });
